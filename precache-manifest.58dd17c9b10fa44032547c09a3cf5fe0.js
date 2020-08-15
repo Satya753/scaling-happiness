2cd0d5ef90d402963facfdbb2088cab5133cc407
Debadutta Padhial <34780428+raffleberry@users.noreply.github.com>
2020-08-15 19:19:40 +0530

    Add files via upload

---
+++ precache-manifest.58dd17c9b10fa44032547c09a3cf5fe0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37a30e8b8775a5181f4b99e394a96bb0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7b68d2289d13dab12e15",
+    "url": "./static/js/2.5579b2b7.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "./static/js/2.5579b2b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14002b8b44becb6a2931",
+    "url": "./static/js/main.0ad8452b.chunk.js"
+  },
+  {
+    "revision": "83f3759b891b4576bb55",
+    "url": "./static/js/runtime-main.1ce99b29.js"
+  }
+]);
