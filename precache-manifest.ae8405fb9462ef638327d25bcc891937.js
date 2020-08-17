0b1db230029ac05ba7e55f00df60d5d8f1497863
raffleberry <raffleberry42@gmail.com>
2020-08-17 23:18:01 +0530

    update

---
+++ precache-manifest.ae8405fb9462ef638327d25bcc891937.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8eb8589c4c175175c694e5b2fb012a9",
+    "url": "/scaling-happiness/index.html"
+  },
+  {
+    "revision": "2109fd4e33ef2cb5519d",
+    "url": "/scaling-happiness/static/js/2.54b42001.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/scaling-happiness/static/js/2.54b42001.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4b8960ffc0133849028",
+    "url": "/scaling-happiness/static/js/main.ba69b6f0.chunk.js"
+  },
+  {
+    "revision": "45756ef1fcd3ef3b649c",
+    "url": "/scaling-happiness/static/js/runtime-main.6deb5096.js"
+  }
+]);
